dd81f35d88dbff73e7aab3ddbba3614ce1255e50
Brian Chau <brian.chau@outlook.com>
2022-05-27 13:55:08 -0700

    Reverted hamming.py back to original version

--- 2021/Day3/hamming.py
+++ 2021/Day3/hamming.py
@@ -4,22 +4,18 @@ if __name__=='__main__':
     with open( 'input.txt' ) as f:
         lines = f.readlines()
 
-    start = timeit.default_timer()
-    for i in range( 10_000 ):
-        gamma, eps = 0, 0
-        first_line = lines[0].strip()
-        line_length = len( first_line )
-        for i in range( line_length ):
-            gamma <<= 1
-            eps   <<= 1
+    gamma, eps = 0, 0
+    first_line = lines[0].strip()
+    line_length = len( first_line )
+    for i in range( line_length ):
+        gamma <<= 1
+        eps   <<= 1
 
-            bits = [ line[i] for line in lines ]
-            ones = bits.count("1")
-            if ones > 500:
-                gamma += 1
-            else:
-                eps   += 1
-    stop = timeit.default_timer()
+        bits = [ line[i] for line in lines ]
+        ones = bits.count("1")
+        if ones > 500:
+            gamma += 1
+        else:
+            eps   += 1
 
     print( gamma * eps )
-    print( stop - start )
