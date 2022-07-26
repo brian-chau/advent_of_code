4b32a88a4d95a7faa145ded75e5ef0c265588fd6
Brian Chau <brian.chau@outlook.com>
2022-07-25 23:02:27 -0700

    Updated 2021/Day2

--- 2021/Day2/sources/main.cpp
+++ 2021/Day2/sources/main.cpp
@@ -10,7 +10,7 @@ int Part1() {
     std::string line;
 
     unsigned int horiz(0);
-    int vert(0);
+    int depth(0);
     for (unsigned int i = 0; std::getline(infile, line); i++) {
         std::stringstream ss(line);
         std::istream_iterator<std::string> begin(ss);
@@ -20,17 +20,46 @@ int Part1() {
         if (vstrings[0] == "forward") {
             horiz += atoi(vstrings[1].c_str());
         } else if (vstrings[0] == "up") {
-            vert -= atoi(vstrings[1].c_str());
+            depth -= atoi(vstrings[1].c_str());
         } else if (vstrings[0] == "down") {
-            vert += atoi(vstrings[1].c_str());
+            depth += atoi(vstrings[1].c_str());
         }
     }
 
-    return horiz * vert;
+    return horiz * depth;
+}
+
+int Part2() {
+    std::ifstream infile("input.txt");
+    std::string line;
+
+    unsigned int horiz(0);
+    int depth(0);
+    int aim(0);
+
+    for (unsigned int i = 0; std::getline(infile, line); i++) {
+        std::stringstream ss(line);
+        std::istream_iterator<std::string> begin(ss);
+        std::istream_iterator<std::string> end;
+        std::vector<std::string> vstrings(begin, end);
+
+        if (vstrings[0] == "forward") {
+            horiz += atoi(vstrings[1].c_str());
+            depth += aim * atoi(vstrings[1].c_str());
+        } else if (vstrings[0] == "up") {
+            depth -= atoi(vstrings[1].c_str());
+            aim -= atoi(vstrings[1].c_str());
+        } else if (vstrings[0] == "down") {
+            depth += atoi(vstrings[1].c_str());
+            aim += atoi(vstrings[1].c_str());
+        }
+    }
+
+    return horiz * depth;
 }
 
 int main() {
     std::cout << Part1() << std::endl;
-    // std::cout << Part2() << std::endl;
+    std::cout << Part2() << std::endl;
     return 0;
 }
